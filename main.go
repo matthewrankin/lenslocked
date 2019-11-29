9813aedfbc28e35bb4926529ae09ecf27bde34d4
Matthew Rankin <matthew@cumulusware.com>
2019-11-29 11:30:03 -0600

    Completed Ch 3

--- main.go
+++ main.go
@@ -18,9 +18,19 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		"support@lenslocked.com</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>FAQ</h1>")
+}
+
+var handle404 http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Page Not Found :(</h1>")
+})
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = handle404
 	http.ListenAndServe(":3000", r)
 }
