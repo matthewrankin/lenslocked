64fea62de2753369a90f2ce0e03e9bcd56efe217
Matthew Rankin <matthew@cumulusware.com>
2019-11-29 11:41:36 -0600

    Completed Section 6.1

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/matthewrankin/lenslocked
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/gorilla/mux v1.7.3
+)
--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var homeTemplate *template.Template
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +32,11 @@ var handle404 http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *htt
 })
 
 func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
