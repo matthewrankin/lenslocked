9b608fd4b2e4f4a5002a7ec368b3ba2ad5e839eb
Matthew Rankin <matthew@cumulusware.com>
2019-12-02 07:07:37 -0600

    Completed Section 9.5

--- exp/main.go
+++ exp/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/matthewrankin/lenslocked/models"
 )
 
 const (
@@ -22,56 +18,40 @@ func main() {
 	dbInfo := fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := gorm.Open("postgres", dbInfo)
+	us, err := models.NewUserService(dbInfo)
 	panicOn(err)
-	defer db.Close()
-	db.LogMode(true)
-	db.AutoMigrate(&User{}, &Order{})
+	defer us.Close()
+	us.DestructiveReset()
 
-	// Get the first user.
-	var user User
-	db.First(&user)
-	panicOn(db.Error)
+	// Create a user
+	user := models.User{
+		Name:  "Michael Scott",
+		Email: "michael@dundermifflin.com",
+	}
+	err = us.Create(&user)
+	panicOn(err)
 
-	// Create a few fake orders.
-	createOrder(db, user, 1001, "Fake Description #1")
-	createOrder(db, user, 9999, "Fake Description #2")
-	createOrder(db, user, 8800, "Fake Description #3")
-}
+	// Get user 1.
+	foundUser, err := us.ByID(1)
+	panicOn(err)
+	fmt.Println(foundUser)
 
-// User models a user.
-type User struct {
-	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
-}
+	// Update a user.
+	user.Name = "Updated Name"
+	err = us.Update(&user)
+	panicOn(err)
+	foundUser, err = us.ByEmail("michael@dundermifflin.com")
+	panicOn(err)
+	fmt.Println(foundUser)
 
-// Order models an order placed by a user.
-type Order struct {
-	gorm.Model
-	UserID      uint
-	Amount      int
-	Description string
-}
+	// Delete a user.
+	err = us.Delete(foundUser.ID)
+	panicOn(err)
+	_, err = us.ByID(foundUser.ID)
+	if err != models.ErrNotFound {
+		panic("user was not deleted!")
+	}
 
-func createOrder(db *gorm.DB, user User, amount int, desc string) {
-	db.Create(&Order{
-		UserID:      user.ID,
-		Amount:      amount,
-		Description: desc,
-	})
-	panicOn(db.Error)
-}
-
-func getInfo() (name, email string) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("What is your name?")
-	name, _ = reader.ReadString('\n')
-	name = strings.TrimSpace(name)
-	fmt.Println("What is your email?")
-	email, _ = reader.ReadString('\n')
-	email = strings.TrimSpace(email)
-	return name, email
 }
 
 func panicOn(err error) {
